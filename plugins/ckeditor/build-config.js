b450e90253ed1e328d0cd36342385a751f430cd4
azamatalt <108387789+azamatalt@users.noreply.github.com>
2022-06-29 17:05:24 +0600

    ckeditor upload

---
+++ plugins/ckeditor/build-config.js
@@ -0,0 +1,108 @@
+﻿/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license/
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) https://ckeditor.com/cke4/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) https://ckeditor.com/cke4/builder/03ae077270064d4e6d6525ba9ffa5bff
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) https://ckeditor.com/cke4/builder/download/03ae077270064d4e6d6525ba9ffa5bff
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono-lisa',
+	preset: 'standard',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.npm',
+		'.nvmrc',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package-lock.json',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'autogrow' : 1,
+		'autolink' : 1,
+		'basicstyles' : 1,
+		'bidi' : 1,
+		'clipboard' : 1,
+		'colorbutton' : 1,
+		'colordialog' : 1,
+		'contextmenu' : 1,
+		'copyformatting' : 1,
+		'dialogadvtab' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'filebrowser' : 1,
+		'floatingspace' : 1,
+		'font' : 1,
+		'format' : 1,
+		'htmlwriter' : 1,
+		'image' : 1,
+		'image2' : 1,
+		'indentblock' : 1,
+		'indentlist' : 1,
+		'justify' : 1,
+		'lineheight' : 1,
+		'link' : 1,
+		'list' : 1,
+		'magicline' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'quicktable' : 1,
+		'removeformat' : 1,
+		'showborders' : 1,
+		'specialchar' : 1,
+		'stylescombo' : 1,
+		'tab' : 1,
+		'table' : 1,
+		'tableresize' : 1,
+		'tabletools' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'uploadimage' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'de' : 1,
+		'en' : 1,
+		'es' : 1,
+		'fr' : 1,
+		'it' : 1,
+		'nl' : 1,
+		'pt' : 1
+	}
+};
